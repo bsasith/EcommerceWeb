8ec35fafb7d9aaa2e5463a17ef35e3163c28e01b
bsasith <40947920+bsasith@users.noreply.github.com>
2024-11-24 20:09:30 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ecommerce Web Site</title>
+    <!-- boortrap link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- font awsome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+<<<<<<< HEAD
+        <link rel="stylesheet" href="styles.css">
+=======
+    <link rel="stylesheet" href="styles.css">
+>>>>>>> f64aff2209fbd7e5120ab720d3aa6a1e57a6699b
+</head>
+
+<body>
+    <!-- nav bar -->
+    <div class="container-fluid p-0">
+        <!-- first child -->
+        <nav class="navbar navbar-expand-lg  bg-info">
+            <div class="container-fluid">
+                <img src=".\images\logo.png.png" class="logo">
+                <a class="navbar-brand" href="#">Logo</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Products</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Register</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                        <li class="nav-item">
+<<<<<<< HEAD
+                            
+=======
+
+>>>>>>> f64aff2209fbd7e5120ab720d3aa6a1e57a6699b
+                            <a class="nav-link" href="#"><i class="fa-solid fa-cart-shopping"></i><sup>1</sup></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Total Price 100/=</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+<<<<<<< HEAD
+        <!-- last child -->
+         <div class="bg-info p-3 text-center">
+            <center><p>All Rights Reserved. Sasith Bogahawatte</p></center>
+         </div>
+=======
+        <!-- Second Child -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Welcome Guest</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Login</a>
+                </li>
+
+            </ul>
+        </nav>
+        <!-- third child -->
+        <div class="bg-light">
+            <h3 class="text-center">
+                Hidden Store
+            </h3>
+            <p class="text-center">
+                Communication is the heart of e-commerce
+            </p>
+        </div>
+        <!-- forth Child -->
+        <div class="row">
+            <!-- Products -->
+
+            <div class="col-md-10">
+                <div class="row">
+                    <div class="col-md-4 mb-2">
+                        <div class="card ">
+                            <img class="card-img-top" src="./images/apples.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.</p>
+                                <a href="#" class="btn btn-info">Add to Cart</a>
+                                <a href="#" class="btn btn-secondary">View more</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 mb-2 ">
+                        <div class="card ">
+                            <img class="card-img-top" src="./images/capsicum.jpeg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.</p>
+                                <a href="#" class="btn btn-info">Add to Cart</a>
+                                <a href="#" class="btn btn-secondary">View more</a>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-md-4 mb-2 ">
+                        <div class="card ">
+                            <img class="card-img-top" src="./images/dairy1.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.</p>
+                                <a href="#" class="btn btn-info">Add to Cart</a>
+                                <a href="#" class="btn btn-secondary">View more</a>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-md-4 mb-2">
+                        <div class="card ">
+                            <img class="card-img-top" src="./images/apples.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.</p>
+                                <a href="#" class="btn btn-info">Add to Cart</a>
+                                <a href="#" class="btn btn-secondary">View more</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 mb-2 ">
+                        <div class="card ">
+                            <img class="card-img-top" src="./images/capsicum.jpeg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.</p>
+                                <a href="#" class="btn btn-info">Add to Cart</a>
+                                <a href="#" class="btn btn-secondary">View more</a>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-md-4 mb-2 ">
+                        <div class="card ">
+                            <img class="card-img-top" src="./images/dairy1.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.</p>
+                                <a href="#" class="btn btn-info">Add to Cart</a>
+                                <a href="#" class="btn btn-secondary">View more</a>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+            <!-- side nav -->
+            <div class="col-md-2 bg-secondary p-0">
+                <!-- brands tobe displayed -->
+              <ul class="navbar-nav me-auto text-center" >
+                <li class="nav-item bg-info">
+                    <a href="#" class="nav-link text-light ">
+                       <h4> Delivery Brands</h4>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-light ">
+                       Brand1
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-light ">
+                       Brand2
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-light ">
+                       Brand3
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-light ">
+                       Brand4
+                    </a>
+                </li>
+                
+
+              </ul>
+              <!-- Categories to be displayed -->
+              <ul class="navbar-nav me-auto text-center" >
+                <li class="nav-item bg-info">
+                    <a href="#" class="nav-link text-light ">
+                       <h4> Categories</h4>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-light ">
+                       Category1
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-light ">
+                       Category2
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-light ">
+                       Category3
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-light ">
+                       Category4
+                    </a>
+                </li>
+                
+
+              </ul>
+            </div>
+        </div>
+        <!-- last child -->
+        <div class="bg-info p-3 text-center">
+            <center>
+                <p>All Rights Reserved. Sasith Bogahawatte</p>
+            </center>
+        </div>
+>>>>>>> f64aff2209fbd7e5120ab720d3aa6a1e57a6699b
+    </div>
+    <!-- bootstrap js link -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
